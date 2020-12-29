118509212bb6758a31acdef5757beb0ea6e96d9c
Alin Vlad <alinvlad05@gmail.com>
2020-12-29 14:55:13 +0200

    Initial commit

---
+++ TestSingletonThreaded.java
@@ -0,0 +1,26 @@
+public class TestSingletonThreaded implements Runnable {
+    Thread thread;
+
+    public static void main(String args[]) {
+        TestSingletonThreaded t = new TestSingletonThreaded();
+    }
+
+    public TestSingletonThreaded() {
+        DatabaseThreaded database;
+
+        database = DatabaseThreaded.getInstance("products");
+
+        thread = new Thread(this, "second");
+        thread.start();
+
+        System.out.println("This is the " + database.getName() + " database.");
+    }
+
+    public void run() {
+        DatabaseThreaded database;
+
+        database = DatabaseThreaded.getInstance("employees");
+
+        System.out.println("This is the " + database.getName() + " database.");
+    }
+}
